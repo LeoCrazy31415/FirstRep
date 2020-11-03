78943c79cbc769216677e51a04cb8b5790b6f3c4
LeoCrazy31415 <73246714+LeoCrazy31415@users.noreply.github.com>
2020-11-03 19:39:18 +0300

    Final version 1.0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection.Emit;
 
 class Program
@@ -22,52 +23,74 @@ class Program
         a = Math.Round(a, accuracy);
         b = Math.Round(b, accuracy);
         c = Math.Round(c, accuracy);
-        Solution(a, b, c, accuracy);
+        List<double> roots = Solution(a, b, c, accuracy, maxValueAbs);
+        if (roots.Count == 0)
+        {
+            Console.WriteLine("Это не уравнение!");
+        }
+        else if(roots.Count == 1)
+        {
+            Console.WriteLine($"Корень уравнения: {roots[0]}");
+        }
+        else
+        {
+            if (roots[0] > maxValueAbs)
+            {
+                Console.WriteLine($"Корни уравнения: {roots[0] / Math.Pow(maxValueAbs, 2)}i, {roots[1] / Math.Pow(maxValueAbs, 2)}i");
+            }
+            else
+            {
+                Console.WriteLine($"Корни уравнения: {roots[0]}, {roots[1]}");
+            }
+        }
         Console.ReadKey();
     }
-    static void Solution(double a, double b, double c, int accuracy)
+    static List<double> Solution(double a, double b, double c, int accuracy, int maxValueAbs)
     {
+        List<double> roots = new List<double>();
         double D = Math.Pow(b, 2) - 4 * a * c;
         if (D > 0) //b^2 > 4*a*c
         {
             if (a == 0 && c == 0)
             {
                 double x = 0;
-                Console.WriteLine($"Корень уравнения: {x}");
+                roots.Add(x);
             }
             else if (a == 0)
             {
                 double x = -c / b;
-                Console.WriteLine($"Корень уравнения: {x}");
+                roots.Add(x);
             }
             else if (c == 0)
             {
                 double x1 = 0;
                 double x2 = -b / a;
-                Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+                roots.Add(x1);
+                roots.Add(x2);
             }
             else
             {
                 double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
                 double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
-                Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+                roots.Add(x1);
+                roots.Add(x2);
             }
         }
         else if (D == 0) //b^2 = 4*a*c
         {
             if (b == 0 && a == 0)
             {
-                Console.WriteLine("Это не уравнение!");
+                return roots;
             }
             else if (b == 0 && c == 0)
             {
                 double x = 0;
-                Console.WriteLine($"Корень уравнения: {x}");
+                roots.Add(x);
             }
             else
             {
                 double x = Math.Round(-b / 2 * a, accuracy);
-                Console.WriteLine($"Корень уравнения: {x}");
+                roots.Add(x);
             }
         }
         else //b^2 < 4*a*c --> complex numbers
@@ -76,14 +99,17 @@ class Program
             {
                 double x1 = -Math.Round(Math.Sqrt(-D) / 2 * a, accuracy);
                 double x2 = Math.Round(Math.Sqrt(-D) / 2 * a, accuracy);
-                Console.WriteLine($"Корни уравнения: {x1}i, {x2}i");
+                roots.Add(x1 * Math.Pow(maxValueAbs, 2));
+                roots.Add(x2 * Math.Pow(maxValueAbs, 2));
             }
             else
             {
                 double x1 = -Math.Round(-b + Math.Sqrt(-D) / 2 * a, accuracy);
                 double x2 = Math.Round(-b - Math.Sqrt(-D) / 2 * a, accuracy);
-                Console.WriteLine($"Корни уравнения: {x1}i, {x2}i");
+                roots.Add(x1 * Math.Pow(maxValueAbs, 2));
+                roots.Add(x2 * Math.Pow(maxValueAbs, 2));
             }
         }
+        return roots;
     }
 }
