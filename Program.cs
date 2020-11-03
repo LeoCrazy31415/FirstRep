e5d11b1527819ff4012fa203af5efb1cae2c0b85
LeoCrazy31415 <73246714+LeoCrazy31415@users.noreply.github.com>
2020-11-03 18:32:20 +0300

    Adding complex solution

--- Program.cs
+++ Program.cs
@@ -27,73 +27,62 @@ class Program
     }
     static void Solution(double a, double b, double c, int accuracy)
     {
-        //double D = Math.Pow(b, 2) - 4 * a * c;
-        //if (D > 0 && a == 0 && c == 0)
-        //{
-        //    double x = 0;
-        //    Console.WriteLine("Корень уравнения: " + x);
-        //}
-        //else if (D > 0 && a == 0)
-        //{
-        //    double x = Math.Round(-c / b, accuracy);
-        //    Console.WriteLine("Корень уравнения: " + x);
-        //}
-        //else if (D > 0 && c == 0)
-        //{
-        //    double x1 = 0;
-        //    double x2 = Math.Round(-b / 2, accuracy);
-        //    Console.WriteLine($"Корни уравнения: {x1}, {x2}");
-        //}
-        //else
-        //{
-        //    double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
-        //    double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
-        //    Console.WriteLine($"Корни уравнения: {x1}, {x2}");
-        //}
-        //if (D == 0 && a != 0 && b != 0)
-        //{
-        //    double x = -b / 2 * a;
-        //    Console.WriteLine("Корень уравнения: " + Math.Round(x, accuracy));
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Данное уравнение не имеет действительных корней");
-        //}
         double D = Math.Pow(b, 2) - 4 * a * c;
-        if (a == 0)
+        if (D > 0) //b^2 > 4*a*c
         {
-            if (b == 0) //D = 0
+            if (a == 0 && c == 0)
             {
-                Console.WriteLine("Это не уравнение!");
+                double x = 0;
+                Console.WriteLine($"Корень уравнения: {x}");
             }
-            else //D = b^2
+            else if (a == 0)
+            {
+                double x = -c / b;
+                Console.WriteLine($"Корень уравнения: {x}");
+            }
+            else if (c == 0)
+            {
+                double x1 = 0;
+                double x2 = -b / a;
+                Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+            }
+            else
             {
                 double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
                 double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
                 Console.WriteLine($"Корни уравнения: {x1}, {x2}");
             }
         }
-        else
+        else if (D == 0) //b^2 = 4*a*c
         {
-            if (b == 0)
+            if (b == 0 && a == 0)
             {
-                if (c == 0) //D = 0
-                {
-                    double x = 0;
-                    Console.WriteLine($"Корень уравнения: {x}");
-                }
-                else //D = -4*a*c
-                {
-                    double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
-                    double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
-                    Console.WriteLine($"Корни уравнения: {x1}, {x2}");
-                }
+                Console.WriteLine("Это не уравнение!");
             }
-            else //D = b^2 - 4*a*c
+            else if (b == 0 && c == 0)
             {
-                double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
-                double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
-                Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+                double x = 0;
+                Console.WriteLine($"Корень уравнения: {x}");
+            }
+            else
+            {
+                double x = Math.Round(-b / 2 * a, accuracy);
+                Console.WriteLine($"Корень уравнения: {x}");
+            }
+        }
+        else //b^2 < 4*a*c --> complex numbers
+        {
+            if (b == 0) //D = - 4*a*c
+            {
+                double x1 = -Math.Round(Math.Sqrt(-D) / 2 * a, accuracy);
+                double x2 = Math.Round(Math.Sqrt(-D) / 2 * a, accuracy);
+                Console.WriteLine($"Корни уравнения: {x1}i, {x2}i");
+            }
+            else
+            {
+                double x1 = -Math.Round(-b + Math.Sqrt(-D) / 2 * a, accuracy);
+                double x2 = Math.Round(-b - Math.Sqrt(-D) / 2 * a, accuracy);
+                Console.WriteLine($"Корни уравнения: {x1}i, {x2}i");
             }
         }
     }
