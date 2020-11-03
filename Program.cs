08e7c0b9733bf672b1bb56f5b6abf3cfe8b2d3bb
LeoCrazy31415 <73246714+LeoCrazy31415@users.noreply.github.com>
2020-11-03 10:25:46 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main()
+    {
+        int maxValueAbs = 1000;
+        int accuracy = 3;
+        Console.Write("Введите коэффициенты\na: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        if (Math.Abs(a) > maxValueAbs || Math.Abs(b) > maxValueAbs || Math.Abs(c) > maxValueAbs)
+        {
+            Console.WriteLine("Уравнение имеет слишком большой по модулю коэффициент!");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+        a = Math.Round(a, accuracy);
+        b = Math.Round(b, accuracy);
+        c = Math.Round(c, accuracy);
+        Solution(a, b, c, accuracy);
+        Console.ReadKey();
+    }
+    static void Solution(double a, double b, double c, int accuracy)
+    {
+        double D = Math.Pow(b, 2) - 4 * a * c;
+        if (D > 0 && a == 0 && c == 0)
+        {
+            double x = 0;
+            Console.WriteLine("Корень уравнения: " + x);
+        }
+        else if (D > 0 && a == 0)
+        {
+            double x = Math.Round(-c / b, accuracy);
+            Console.WriteLine("Корень уравнения: " + x);
+        }
+        else if (D > 0 && c == 0)
+        {
+            double x1 = 0;
+            double x2 = Math.Round(-b / 2, accuracy);
+            Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+        }
+        else
+        {
+            double x1 = Math.Round((-b + Math.Sqrt(D)) / 2 * a, accuracy);
+            double x2 = Math.Round((-b - Math.Sqrt(D)) / 2 * a, accuracy);
+            Console.WriteLine($"Корни уравнения: {x1}, {x2}");
+        }
+        if (D == 0 && a != 0 && b != 0)
+        {
+            double x = -b / 2 * a;
+            Console.WriteLine("Корень уравнения: " + Math.Round(x, accuracy));
+        }
+        else
+        {
+            Console.WriteLine("Данное уравнение не имеет действительных корней");
+        }
+    }
+}
